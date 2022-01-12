022cdf948ac67c87f75824f156b85f0c051ed324
moderncraft1234 <66831184+moderncraft1234@users.noreply.github.com>
2022-01-12 14:52:55 +0100

    changes

--- dashboard.php
+++ dashboard.php
@@ -6,14 +6,14 @@ include("auth_session.php");
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Dashboard - Client area</title>
+    <title>Dashboard - Client area<form></form></title>
     <link rel="stylesheet" href="style.css" />
 </head>
 <body>
     <div class="form">
         <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
-        <p>You are in user dashboard page.</p>
+        <p>dit is de toets pagina</p>
         <p><a href="logout.php">Logout</a></p>
     </div>
-</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-header("Location: login.php");
-?>
+
+<body>
+
+
+
+  <body>  
+
+<head>
+
+<link rel="stylesheet" href="style.css" />
+
+<head>
+
+<html>
+
+<div>
+
+<p><a href="login.php">login</a></p>
+
+<p><a href="registration.php"> register </a><p>
+
+  <div>
+
+<html>
