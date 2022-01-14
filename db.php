5777554763dc379e1083e50d124d98539c6994ea
moderncraft1234 <66831184+moderncraft1234@users.noreply.github.com>
2022-01-14 10:06:42 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","4schoolonly","heroku");
+    $con = mysqli_connect("51.195.187.101","root","4schoolonly","schooldatabase");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
